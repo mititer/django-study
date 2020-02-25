969d5f7fe8144dba8faadbea15afa93fd819ef69
Kevin Wang <jinhuawang76@126.com>
2020-02-25 11:14:38 +0800

    migrate to mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,9 +86,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'djangodb',
+        'PASSWORD': 'djangodb',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -141,7 +152,7 @@ LOGGING = {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
             #'filename': 'e:\django.log',
-            'filename': "$HOME/python/django-study/django.log",
+            'filename': "django.log",
         },
     },
     'loggers': {
